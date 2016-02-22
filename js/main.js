c053a8a478402837e0fd76ace54ead19f6e871a6
LordMegaDeath3KHaxxor <marli166@student.liu.se>
2016-02-22 14:13:23 +0100

    Gloabl colors in main.js and removed an unnecessary button.

--- js/main.js
+++ js/main.js
@@ -4,4 +4,15 @@ d3.csv("data/Swedish_Election.csv", function (data) {
 
 	map1 = new map(data);
 
+	// Global colors
+    color = new Map();
+    color.set("Moderaterna", "#52BDEC");
+    color.set("Centerpartiet", "#016A3A");
+    color.set("Folkpartiet", "#0094D7");
+    color.set("Kristdemokraterna", "#231977");
+    color.set("Miljöpartiet", "#53A045");
+    color.set("Socialdemokraterna", "#ED1B34");
+    color.set("Vänsterpartiet", "#DA291C");
+    color.set("Sverigedemokraterna", "#DDDD00");
+    color.set("övriga partier", "gray");
 });
